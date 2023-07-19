31cc392ab94c5577c9b616d22e002e0b998b8e30
kiscean <visocorodnii@gmail.com>
2023-07-19 15:57:22 +0300

    Work19

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,15 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
 import {
+    Dropdown,
+    Space,
+    Col,
     Layout,
     Menu,
+    Row,
     theme
 } from 'antd';
 import SearchEKB from "./search/SearchEKB";
 import CategoriesEKB from "./categories/CategoriesEKB";
 import logo from "../static/img/ekb_portal_logo.png"
-
+import { DownOutlined, SmileOutlined } from '@ant-design/icons';
 
 const { Header, Content, Footer } = Layout;
+
+const items = [
+    {
+        key: '1',
+        label: (
+            <a target="_blank" rel="noopener noreferrer" href="#">
+                Профиль
+            </a>
+        ),
+    },
+    {
+        key: '2',
+        label: (
+            <a target="_blank" rel="noopener noreferrer" href="#">
+                Настройки партнера
+            </a>
+        ),
+        icon: <SmileOutlined />,
+        disabled: true,
+    },
+    {
+        key: '3',
+        label: (
+            <a target="_blank" rel="noopener noreferrer" href="#">
+                История заказов
+            </a>
+        ),
+        disabled: true,
+    },
+    {
+        key: '4',
+        danger: true,
+        label: 'Выйти',
+    },
+];
 
 const Main = () => {
 
@@ -25,6 +64,7 @@ const Main = () => {
     } = theme.useToken();
 
     return (
+        <>
         <Layout className="layout">
             <Header
                 style={{
@@ -32,26 +72,53 @@ const Main = () => {
                     alignItems: 'center',
                 }}
             >
-                <div
+                <Row justify="center"
                     style={{
-                        marginTop: "3.5ch",
-                        marginRight: "10ch",
+                        height: '13ch',
+                        width: '100vw',
+                        alignItems: "center",
                     }}
                 >
-                    <img src={logo} width={"250"} height={"40"}/>
-                </div>
-                <Menu
-                    theme="dark"
-                    mode="horizontal"
-                    defaultSelectedKeys={['4']}
-                    items={MenuItems.map((item) => {
-                        const key = item.Name;
-                        return {
-                            key,
-                            label: `${key}`,
-                        };
-                    })}
-                />
+                    <Col span={8}>
+                        <div
+                            style={{
+                                marginTop: "3.5ch",
+                                marginRight: "10ch",
+                            }}
+                        >
+                            <img src={logo} width={"250"} height={"40"}/>
+                        </div>
+                    </Col>
+                    <Col span={8}>
+                        <Menu
+                            theme="dark"
+                            mode="horizontal"
+                            defaultSelectedKeys={['4']}
+                            items={MenuItems.map((item) => {
+                                const key = item.Name;
+                                return {
+                                    key,
+                                    label: `${key}`,
+                                };
+                            })}
+                        />
+
+                    </Col>
+                    <Col span={8}>
+                        <Dropdown
+                            menu={{
+                                items,
+                            }}
+                        >
+                            <a onClick={(e) => e.preventDefault()}>
+                                <Space>
+                                    Hover me
+                                    <DownOutlined />
+                                </Space>
+                            </a>
+                        </Dropdown>
+                    </Col>
+                </Row>
             </Header>
             <Content
                 style={{
@@ -75,6 +142,7 @@ const Main = () => {
                 Маркетплейс ЭКБ ©2023 Проект АО ЗАСЛОН
             </Footer>
         </Layout>
+        </>
     );
 };
 
