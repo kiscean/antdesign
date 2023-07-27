47fecdd2a75fdb21804f33ec48dc50f3f6434dd7
Anastasia <www.nastya97@yandex.ru>
2023-07-27 21:46:39 +0300

    refactor: fix BEM naming in header without burger menu

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,128 +1,109 @@
-import React, {useState} from 'react';
-import {Dropdown, Space, Col, Layout, Menu, Row, Avatar, Divider} from 'antd';
-import {
-    DownOutlined,
-    ShoppingCartOutlined,
-} from '@ant-design/icons';
+import React from 'react';
+import { Dropdown, Space, Col, Layout, Menu, Row, Avatar, Divider } from 'antd';
+import { DownOutlined, ShoppingCartOutlined } from '@ant-design/icons';
 import { items, menuItems } from '../../assets/constants';
 import './Main.css';
 
 import SearchEKB from '../SearchEKB/SearchEKB';
 import CategoriesEKB from '../CategoriesEKB/CategoriesEKB';
-import PopProducts from "../PopProducts/PopProducts";
-import RightMenu from "../RightMenu/RightMenu";
+import PopProducts from '../PopProducts/PopProducts';
+import RightMenu from '../RightMenu/RightMenu';
 
 const { Header, Content, Footer } = Layout;
 
 const Main = () => {
-
-    const [menuActive, setMenuActive] = useState(false)
+  const [menuActive, setMenuActive] = React.useState(false);
 
   return (
     <>
       <Layout className="layout">
-        <Header
-            className={'navbar'}
-        >
-            <Space direction={"horizontal"} className={'container'}>
-                <Space direction="horizontal">
-                    <div
-                        className={'logo'}
-                    >
-                        <h1>
-                            <a href={'#'} className={'logo__a'}>
-                                ЭКБ
-                            </a>
-                        </h1>
-                    </div>
+        <Header className="header">
+          <Space direction="horizontal" className="header__container">
+            <Space direction="horizontal">
+              <div className="header__logo">
+                <h1>
+                  <a href="#" className="header__logo-link">
+                    ЭКБ
+                  </a>
+                </h1>
+              </div>
 
-                    <div
-                        className={'logo__slogan'}
-                    >
-                        <p>Маркетплейс <br /> для инженеров</p>
-                    </div>
-                </Space>
-                <Col span={24} className='navbar__menu'>
-                    <Menu
-                        theme="dark"
-                        mode="horizontal"
-                        defaultSelectedKeys={['4']}
-                        items={
-                            menuItems.map((item) => {
-                                    const key = item.name;
-                                    return {
-                                        key,
-                                        label: `${key}`,
-                                    };
-                                })}
-                    />
-                </Col>
-                <Space direction={"horizontal"} size={20}>
-                    <ShoppingCartOutlined className={'navbar-basket'}/>
-
-                    <Space className='navbar-user__full'>
-                        <Avatar
-                            className='navbar-user__icon'
-                        >
-                            OR
-                        </Avatar>
-                        <Dropdown
-                            menu={{
-                                items,
-                            }}
-                        >
-                            <Space className={'navbar-user__name'}>
-                                МояКомпания
-                                <DownOutlined />
-                            </Space>
-                        </Dropdown>
-                    </Space>
-
-                    <div className='navbar-user__rightmenu' onClick={() => setMenuActive(!menuActive)}>
-                        <Avatar
-                            className='navbar-user__icon'
-                        >
-                            OR
-                        </Avatar>
-                        <DownOutlined className='navbar-user__rightmenuicon'/>
-                    </div>
-
-                </Space>
+              <span className="header__logo-description">
+                Маркетплейс <br /> для инженеров
+              </span>
             </Space>
+
+            <Col span={24} className="header__menu">
+              <Menu
+                theme="dark"
+                mode="horizontal"
+                defaultSelectedKeys={['4']}
+                items={menuItems.map((item) => {
+                  const key = item.name;
+                  return {
+                    key,
+                    label: `${key}`,
+                  };
+                })}
+              />
+            </Col>
+            <Space direction="horizontal" size={20}>
+              <ShoppingCartOutlined className="header__basket-logo" />
+
+              <Space className="header__user-info">
+                <Avatar className="header__user-icon">MC</Avatar>
+                <Dropdown
+                  menu={{
+                    items,
+                  }}>
+                  <Space className="header__user-name">
+                    МояКомпания
+                    <DownOutlined />
+                  </Space>
+                </Dropdown>
+              </Space>
+
+              {/*  */}
+              <div
+                className="navbar-user__rightmenu"
+                onClick={() => setMenuActive(!menuActive)}>
+                <Avatar className="navbar-user__icon">OR</Avatar>
+                <DownOutlined className="navbar-user__rightmenuicon" />
+              </div>
+            </Space>
+          </Space>
         </Header>
-        <RightMenu active={menuActive} setActive={setMenuActive}/>
+        <RightMenu active={menuActive} setActive={setMenuActive} />
 
         <Content style={{ textAlign: 'center' }}>
           <SearchEKB />
         </Content>
 
         <Content style={{ textAlign: 'center' }}>
-            <PopProducts />
+          <PopProducts />
         </Content>
 
-        <Content style={{ textAlign: "center" }}>
+        <Content style={{ textAlign: 'center' }}>
           <CategoriesEKB />
         </Content>
 
         <Footer style={{ textAlign: 'center' }}>
-            <Row gutter={1}>
-                <Col span={24}>
-                    Маркетплейс ЭКБ &copy; 2023 ООО Компания
-                </Col>
-                <Col span={24}
-                    style={{
-                        marginTop: '2ch',
-                    }}
-                >
-                    <a href="#">О проекте</a>
-                    <Divider type="vertical" />
-                    <a href="#">Карьера</a>
-                    <Divider type="vertical" />
-                    <a href="#">Сертификаты</a>
-                    <Divider type="vertical" />
-                    <a href="#">Тех поддержка</a>
-                </Col>
-            </Row>
+          <Row gutter={1}>
+            <Col span={24}>Маркетплейс ЭКБ &copy; 2023 ООО Компания</Col>
+            <Col
+              span={24}
+              style={{
+                marginTop: '2ch',
+              }}>
+              <a href="#">О проекте</a>
+              <Divider type="vertical" />
+              <a href="#">Карьера</a>
+              <Divider type="vertical" />
+              <a href="#">Сертификаты</a>
+              <Divider type="vertical" />
+              <a href="#">Тех поддержка</a>
+            </Col>
+          </Row>
         </Footer>
       </Layout>
     </>
