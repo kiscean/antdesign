2efef69c74d7432495ae9d23c56c16081e7af220
kiscean <konstantin-s-k@yandex.ru>
2023-08-08 00:40:31 +0300

    Home 07-08

--- src/components/SearchResults/ComponentSearchResults.js
+++ src/components/SearchResults/ComponentSearchResults.js
@@ -6,31 +6,65 @@ import Header from "../Header/Header";
 import SearchEKB from "../SearchEKB/SearchEKB";
 
 import { Content } from "antd/es/layout/layout";
-import {DownloadOutlined, HomeOutlined} from '@ant-design/icons';
-import { Breadcrumb, Pagination, Radio, Space, Table, Tag } from "antd";
+import {
+    CheckOutlined,
+    FilePdfFilled,
+    HeartFilled,
+    HeartOutlined,
+    HomeOutlined,
+    PlusOutlined
+} from '@ant-design/icons';
+import {Breadcrumb, Col, Divider, Rate, Row, Space, Table, Tag} from "antd";
 
 const ComponentSearchResults = () => {
-
     const columns = [
         {
             title: 'Компонент',
             dataIndex: 'name',
             key: 'name',
-            render: (text) => <a>{text}</a>,
+            render: (_, record) => (
+                <Space size="middle">
+                    <img src={record.image} className='component__image' />
+                    <Row>
+                        <Col span={24}>
+                            <a>{record.name}</a>
+                        </Col>
+                        <Col span={24}>
+                            <p>{record.discription}</p>
+                        </Col>
+                    </Row>
+                </Space>
+            ),
+            width: '70ch',
+
         },
         {
             title: 'Файл',
             key: 'file',
             render: (_, record) => (
                 <Space size="middle">
-                    <a><DownloadOutlined />{record.file}</a>
+                    {
+                        record.file==='PDF' ? <a><FilePdfFilled /> PDF</a> : ''
+                    }
                 </Space>
-            )
+            ),
+            width: '10ch'
+        },
+        {
+            title: 'Рейтинг',
+            dataIndex: 'rating',
+            key: 'rating',
+            render: (_, record) => (
+                <Rate disabled defaultValue={record.rating} className='component__rate'/>
+            ),
+            width: '19ch',
+            sorter: (a, b) => a.rating - b.rating,
         },
         {
             title: 'Производитель',
             dataIndex: 'production',
             key: 'production',
+            sorter: (a, b) => a.production.length - b.production.length,
         },
         {
             title: 'Теги',
@@ -44,50 +78,195 @@ const ComponentSearchResults = () => {
                 color = 'volcano';
             }
             return (
-                <Tag color={color} key={tag}>
+                <Tag color={color} key={tag} className='component__tag'>
                     {tag.toUpperCase()}
                 </Tag>
             );
         })}
       </span>
             ),
+            width: '20ch',
         },
         {
             title: 'Покупка',
             key: 'action',
             render: (_, record) => (
                 <Space size="middle">
-                    <a>Добавить в корзину</a>
+                    {
+                        record.action==='1' ? <a className='component__backet-active'><CheckOutlined /> В корзине</a> : <a><PlusOutlined /> Добавить в корзину</a>
+                    }
                 </Space>
             ),
-        },
-    ];
-    const data = [
-        {
-            key: '1',
-            name: 'John Brown',
-            file: 'PDF',
-            production: 'New York No. 1 Lake Park',
-            tags: ['в наличии', 'быстрая доставка'],
+            width: '25ch'
         },
         {
-            key: '2',
-            name: 'Jim Green',
-            file: '',
-            production: 'London No. 1 Lake Park',
-            tags: ['под заказ'],
-        },
-        {
-            key: '3',
-            name: 'Joe Black',
-            file: 'PDF',
-            production: 'Sydney No. 1 Lake Park',
-            tags: ['в наличии', 'быстрая доставка'],
+            title: 'Избранное',
+            key: 'heart',
+            render: (_, record) => (
+                <Space size="middle">
+                    {
+                        record.heart==='1' ? <a><HeartFilled className='component__heart-active'/></a> : <a><HeartOutlined /></a>
+                    }
+                </Space>
+            ),
+            width: '5ch'
         },
     ];
 
-    const [top, setTop] = useState('topRight');
-    const [bottom, setBottom] = useState('bottomRight');
+    const data = [
+        {
+            key: '1',
+            image: 'https://static.chipdip.ru/lib/211/DOC001211284.jpg',
+            name: 'К10-17А М47',
+            discription: 'Конденсатор керамический выводной М47 6.8пФ, 20%',
+            file: 'PDF',
+            rating: '3',
+            production: 'Россия, ОАО Завод деталей',
+            tags: ['в наличии', 'быстрая доставка'],
+            action: '',
+            heart: '1',
+        },
+        {
+            key: '2',
+            image: 'https://static.chipdip.ru/lib/294/DOC005294472.jpg',
+            name: '78D05L',
+            discription: 'Стабилизатор напряжения +5В 1А',
+            file: 'PDF',
+            rating: '4',
+            production: 'USA, ElectricAdvence',
+            tags: ['под заказ'],
+            action: '',
+            heart: '0',
+        },
+        {
+            key: '3',
+            image: 'https://static.chipdip.ru/lib/313/DOC005313873.jpg',
+            name: '1N4148WS-7-F',
+            discription: 'Диод 150мА 75В [SOD-323]',
+            file: 'PDF',
+            rating: '4',
+            production: 'Россия, ОАО Завод деталей',
+            tags: ['в наличии', 'быстрая доставка'],
+            action: '1',
+            heart: '0',
+        },
+        {
+            key: '4',
+            image: 'https://static.chipdip.ru/lib/464/DOC002464721.jpg',
+            name: '2А517А-2',
+            discription: 'Диод СВЧ',
+            file: 'PDF',
+            rating: '3',
+            production: 'Россия, ОАО Завод деталей',
+            tags: ['в наличии', 'быстрая доставка'],
+            action: '1',
+            heart: '0',
+        },
+        {
+            key: '5',
+            image: 'https://static.chipdip.ru/lib/036/DOC024036278.jpg',
+            name: 'BAR6304WH6327XTSA1',
+            discription: 'Электроэлемент PIN DIODE, AEC-Q101, 50V, 0.1A, SOT-323',
+            file: '',
+            rating: '5',
+            production: 'USA, ElectricAdvence',
+            tags: ['под заказ'],
+            action: '',
+            heart: '1',
+        },
+        {
+            key: '6',
+            image: 'https://static.chipdip.ru/lib/243/DOC004243462.jpg',
+            name: 'TDR-240-24',
+            discription: 'Блок питания, вход: 3-фазное 340-550В, выход: 24В,10А,240Вт',
+            file: 'PDF',
+            rating: '4',
+            production: 'Россия, ОАО Завод деталей',
+            tags: ['в наличии', 'быстрая доставка'],
+            action: '',
+            heart: '1',
+        },
+        {
+            key: '7',
+            image: 'https://static.chipdip.ru/lib/304/DOC005304605.jpg',
+            name: 'TZC3P200A110',
+            discription: '5-20пФ, SMD конденсатор подстроечный',
+            file: 'PDF',
+            rating: '4',
+            production: 'Россия, ОАО Завод деталей',
+            tags: ['в наличии', 'быстрая доставка'],
+            action: '',
+            heart: '0',
+        },
+        {
+            key: '8',
+            image: 'https://static.chipdip.ru/lib/337/DOC004337152.jpg',
+            name: 'JYC3D471KBB',
+            discription: '470 пФ, 2 кВ, 10%, Y5P, D=6.5мм, Конденсатор керамический дисковый',
+            file: '',
+            rating: '1',
+            production: 'USA, ElectricAdvence',
+            tags: ['под заказ'],
+            action: '',
+            heart: '0',
+        },
+        {
+            key: '9',
+            image: 'https://static.chipdip.ru/lib/638/DOC021638278.jpg',
+            name: 'CK45-R3DD102K-VRAF',
+            discription: 'Конденсатор керамический дисковый, напряжение 2000, емкость 1000P, отклонение K, вид керамики R',
+            file: 'PDF',
+            rating: '4',
+            production: 'USA, ElectricAdvence',
+            tags: ['под заказ'],
+            action: '',
+            heart: '0',
+        },
+        {
+            key: '10',
+            image: 'https://static.chipdip.ru/lib/193/DOC001193601.jpg',
+            name: 'HC-49U',
+            discription: 'Кварцевый резонатор',
+            file: 'PDF',
+            rating: '4',
+            production: 'USA, ElectricAdvence',
+            tags: ['под заказ'],
+            action: '',
+            heart: '0',
+        },
+        {
+            key: '11',
+            image: 'https://static.chipdip.ru/lib/638/DOC021638278.jpg',
+            name: 'CK45-R3DD102K-VRAF',
+            discription: 'Конденсатор керамический дисковый, напряжение 2000, емкость 1000P, отклонение K, вид керамики R',
+            file: 'PDF',
+            rating: '3',
+            production: 'USA, ElectricAdvence',
+            tags: ['под заказ'],
+            action: '',
+            heart: '0',
+        },
+        {
+            key: '12',
+            image: 'https://static.chipdip.ru/lib/256/DOC031256591.jpg',
+            name: 'К53-4Т',
+            discription: '3.3мкФ 16в 20%, Конденсатор ниобиевый оксидно-полупроводниковый',
+            file: 'PDF',
+            rating: '3',
+            production: 'Россия, ОАО Завод деталей',
+            tags: ['в наличии', 'быстрая доставка'],
+            action: '',
+            heart: '1',
+        },
+
+    ];
+
+    const [tableParams, setTableParams] = useState({
+        pagination: {
+            current: 1,
+            pageSize: 10,
+        },
+    });
 
     return (
         <section>
@@ -114,32 +293,20 @@ const ComponentSearchResults = () => {
                         },
                     ]}
                 />
+                <section className='category-nestedlist'>
+                    <Divider orientation="left"><h4>Оптоэлектроника</h4></Divider>
+
+
+                </section>
+                <section className='search-results'>
+                    <div>
+                        <Table
+                            columns={columns}
+                            dataSource={data}
+                        />
+                    </div>
+                </section>
             </Content>
-            <section className='category-nestedlist'>
-                <h3>Оптоэлектроника</h3>
-
-
-            </section>
-            <section className='search-results'>
-                <div>
-                    <Table
-                        columns={columns}
-                        pagination={{
-                            position: [top, bottom],
-                            pageSize: 10,
-                            current: 1,
-                        }}
-                        dataSource={data}
-                    />
-                </div>
-
-                <Pagination
-                    total={85}
-                    showSizeChanger
-                    showQuickJumper
-                    showTotal={(total) => `Total ${total} items`}
-                />
-            </section>
             <Footer />
         </section>
     );
