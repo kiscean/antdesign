6b5c0e809751274311b734cafabbd7fb9f61b0e1
Anastasia <www.nastya97@yandex.ru>
2023-09-13 20:07:50 +0300

    feat: full 404 without adaptive

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './PageNotFound.css';
+import { Link } from 'react-router-dom';
 
 const PageNotFound = () => {
   return (
     <div className="not-found">
       <h2 className="not-found__code">404</h2>
       <div className="not-found__right">
-        <p className="not-found__error">К сожалению этой страницы нет</p>
+        <p className="not-found__error">
+          К сожалению такой страницы не существует
+        </p>
         <p className="not-found__paragraph">
           Зато у нас много других интересных страниц. Вернитесь на{' '}
-          <a className="not-found__link">главную</a>, чтобы с ними ознакомиться
+          <Link to="/" className="not-found__link">
+            главную
+          </Link>
+          , чтобы с ними ознакомиться
         </p>
       </div>
     </div>
